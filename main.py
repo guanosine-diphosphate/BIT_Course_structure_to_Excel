015a8e3e2844c521a147d59ef5f279d4d72e4364
KamisatoAyaka <youyieoee@qq.com>
2024-07-17 15:10:16 +0800

    modified logic to get top line

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
-from bs4 import BeautifulSoup
 import pandas as pd
-def process(file:str):
-    f=open(file,'r',encoding="utf-8").read()
+from bs4 import BeautifulSoup
+
+
+def process(file: str):
+    f = open(file, 'r', encoding="utf-8").read()
     # print(f)
-    soup = BeautifulSoup(f,'html.parser')
-    trs=soup.find_all('tr')
-    courseList=[]
+    soup = BeautifulSoup(f, 'html.parser')
+    trs = soup.find_all('tr')
+    # print(trs[1])
+    colums_raw = trs[1].find_all('th')
+    colums = []
+    for colum in colums_raw:
+        colums.append(colum.text)
+    courseList = []
     for tr in trs:
-        course=[]
-        tds=tr.find_all('td')
+        course = []
+        tds = tr.find_all('td')
         for td in tds:
             course.append(td.text)
         courseList.append(course)
-    courseList=courseList[2:]
-    return courseList
-def writeExcel(courseList):
-    colums="序号,开课学期,课程编号,课程名称,开课单位,学分,总学时,考核方式,课程属性,课程性质,是否考试".split(',')
+    courseList = courseList[2:]
+    return (courseList, colums)
+
+
+def writeExcel(input):
+    courseList = input[0]
+    colums = input[1]
     print(colums)
-    pd_list=pd.DataFrame(courseList,columns=colums)
-    pd_list.to_excel('培养方案.xlsx',index=False)
+    pd_list = pd.DataFrame(courseList, columns=colums)
+    pd_list.to_excel('培养方案.xlsx', index=False)
+
+
 if __name__ == '__main__':
-    data=process("html.txt")
+    data = process("html.txt")
     writeExcel(data)
